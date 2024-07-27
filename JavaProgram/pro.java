358aace2ce533500587b9e6d2e7ae0ae6791b797
deepsa99 <learningswithds@gmail.com>
2024-07-27 15:23:37 +0530

    moving a file for first time to git

---
+++ JavaProgram/pro.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter num:");
+        int num = sc.nextInt();
+        
+        // Optional: You can add some functionality to use the 'num' variable.
+        System.out.println("You entered: " + num);
+    }
+}
+
+
