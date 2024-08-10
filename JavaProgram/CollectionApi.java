832126d2d3b9e5860eae81d470a7ffbc0927a770
Sarangantth <sarangantth@gmail.com>
2024-08-10 16:16:44 +0530

    map function

--- JavaProgram/CollectionApi.java
+++ JavaProgram/CollectionApi.java
@@ -1,7 +1,18 @@
 package JavaProgram;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CollectionApi {
     public static void main(String a[]){
-        
+        Map<String, Integer> Students = new HashMap<>();
+        Students.put("Sarangantth", 99);
+        Students.put("Vijay", 69);
+        Students.put("Deepika", 100);
+        Students.put("Rakesh", 79);
+
+        for(String key : Students.keySet()){
+            System.out.println(key + " : " + Students.get(key));
+        }
     }
 }
